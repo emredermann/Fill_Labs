4f0ea89107a076e6488e96772339ab055d9c10a3
6emre <emre.derman@ug.bilkent.edu.tr>
2022-11-16 19:23:39 +0300

    First 3 questions added to the project file.

--- fill_labs_application.go
+++ fill_labs_application.go
@@ -4,9 +4,17 @@ import(
 	"fmt"
 	)
 	/*
-		Q1) Write a function that sorts a bunch of words by the number of character “a”s within the
+	Q1) Write a function that sorts a bunch of words by the number of character “a”s within the
 		word (decreasing order). If some words contain the same amount of character “a”s then you
 		need to sort those words by their lengths.
+	*/
+	/*
+	Q2) Write a recursive function which takes one integer parameter. Please bear in mind that
+		finding the algorithm needed to generate the output below is the main point of the question.
+	*/
+	/*
+	Q3) Write a function which takes one parameter as an array/list. Find most repeated data
+		within a given array.
 	*/
 func main() {
     fmt.Println("!... Hello World ...!")
