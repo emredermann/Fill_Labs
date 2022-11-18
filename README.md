5b001155865995ca75ff18f5e40d4f285e41f994
Emre derman <emre.derman@ug.bilkent.edu.tr>
2022-11-18 23:49:39 +0300

    go language warm up initial solution for question 1.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Fill_Labs_Interview
 README file will be updated as soon as  company approved.
+
+
+### In order to run project
+"go run main.go "
--- fill_labs_application.go
+++ fill_labs_application.go
@@ -2,6 +2,8 @@ package main
 
 import(
 	"fmt"
+	"strings"
+	"sort"
 	)
 	/*
 	Q1) Write a function that sorts a bunch of words by the number of character “a”s within the
@@ -16,6 +18,24 @@ import(
 	Q3) Write a function which takes one parameter as an array/list. Find most repeated data
 		within a given array.
 	*/
+
+func q_1(){
+	var theArray  [3]string
+	theArray[0] = "India"  // Assign a value to the first element
+	theArray[1] = "Canada" // Assign a value to the second element
+	theArray[2] = "Japan"  // Assign a value to the third element
+	// for x := 0; x < len(theArray); x++ {
+    //     fmt.Println(strings.Count(theArray[x],"a"))
+	// 	append(theCounterArray, strings.Count(theArray[x],"a"))
+    // }
+	// fmt.Println(theCounterArray)
+	fmt.Println("Before sorting  " ,theArray)
+	sort.SliceStable(theArray, func(i, j int) bool {
+		return strings.Count(theArray[i],"a") < strings.Count(theArray[j],"a")
+	})
+	fmt.Println(theArray)
+}
 func main() {
     fmt.Println("!... Hello World ...!")
+	q_1()
 }
